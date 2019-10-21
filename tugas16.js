495fbf01e912828584a5c67cf5820dd32aeabacf
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-22 01:00:56 +0700

    tugas 16 muhammad rizwan

---
+++ tugas16.js
@@ -0,0 +1,13 @@
+/* tugas 16 javascript */
+// Muhammad Rizwan
+
+function tugasEnamBelas(){
+
+    var min = Math.min(45,100,99,3,2,4,1,2,3,19,22,14,10);
+    console.log(min);
+
+    var max = Math.max(45,100,99,3,2,4,1,2,3,19,22,14,10);
+    console.log(max);
+}
+
+tugasEnamBelas();
