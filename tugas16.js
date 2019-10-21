f847f303d3d336203017e85ddfe561e1cb9d320e
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-22 01:03:34 +0700

    tugas 16 javascript muhammad rizwan

--- tugas16.js
+++ tugas16.js
@@ -4,10 +4,10 @@
 function tugasEnamBelas(){
 
     var min = Math.min(45,100,99,3,2,4,1,2,3,19,22,14,10);
-    console.log(min);
+    console.log('Nilai Minimal:',min);
 
     var max = Math.max(45,100,99,3,2,4,1,2,3,19,22,14,10);
-    console.log(max);
+    console.log('Nilai Maksimal:',max);
 }
 
 tugasEnamBelas();
